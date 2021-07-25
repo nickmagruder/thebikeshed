6ea7e9eb4c3d92f4afbd2ebc63fabf8ddf4a11a0
nickmagruder <nick.magruder@outlook.com>
2021-07-24 17:26:30 -0700

    signin/signup forms started

---
+++ src/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-and-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+
+  <div className='sign-in-and-up'>
+    <SignIn />
+  </div>
+
+)
+
+export default SignInAndSignUpPage;
