8cda6a9266ba9c2284a32e5ae1b9f76ab74abac4
nickmagruder <nick.magruder@outlook.com>
2021-10-06 22:35:31 -0700

    collection wip

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 - 7/2 - 1:56 - Menu item styling/animations added
 - 7/9 6:40pm - Started building routes - Hats route created
 - 7/19 11:22pm - Collection preview component created
+- 7/24 - Shop page and sign-in/up started
+- 7/29 - 
+
---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './form-input.styles.scss';
+
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+
+  <div className='group'>
+
+    <input className='form-input' onChange={handleChange} {...otherProps} />
+
+    {
+      label ?
+        (<label className={`${props.value.length ? 'shrink' : ''} `}>
+
+        </label>)
+        : null
+    }
+
+
+  </div>
+
+
+)
