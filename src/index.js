c8acd07761e36519c4cbbab9157b2025d5e1b5a5
nickmagruder <nick.magruder@outlook.com>
2021-10-12 20:46:40 -0700

    sign up built

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -8,9 +8,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <browserRouter>
+    <BrowserRouter>
       <App />
-    </browserRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
