41d6120e52203b93790865e03fba505b8a5f7822
nickmagruder <nick.magruder@outlook.com>
2021-10-21 13:59:01 -0700

    no images glitch fixed

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
+const CollectionItem = ({ id, name, imageUrl, price }) => (
+  <div key={id} className='collection-item'>
     <div
       className='image'
       style={{
