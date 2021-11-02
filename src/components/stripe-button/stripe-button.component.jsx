9bd87f51f223218d2738ef4785541137af5d98bf
nickmagruder <nick.magruder@outlook.com>
2021-11-02 16:16:28 -0700

    stripe integration

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,2 +1,26 @@
-import React from 'react';
+/* import React from 'react';
 
+const StripeCheckOutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51JpfUGKJk1wg0EyFVMUtuMID9q12kg9bW4v1vdA1opeGXkZpXjVXdNpUMcU0ZsZR8ETU2HRsT6npRsPSrT2zUGgF00CyMjRrjA';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='NMagruder Ecommerce'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+} */
