b215442b4071c7c9ae92033c77efd4c396738592
_NickMagruder <71102316+nickmagruder@users.noreply.github.com>
2025-09-22 19:14:17 -0700

    Map enhancements

--- src/pages/contact/contact.component.tsx
+++ src/pages/contact/contact.component.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -9,15 +9,40 @@ const Contact: FC = () => {
   const mapRef = useRef<mapboxgl.Map | null>(null);
   const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
+  const INITIAL_CENTER: [number, number] = [-122.3493, 47.6205];
+  const INITIAL_ZOOM = 10.12;
+
+  const [center, setCenter] = useState<[number, number]>(INITIAL_CENTER);
+  const [zoom, setZoom] = useState(INITIAL_ZOOM);
+
+  const handleButtonClick = () => {
+    mapRef.current?.flyTo({
+      center: INITIAL_CENTER,
+      zoom: INITIAL_ZOOM,
+    });
+  };
+
   useEffect(() => {
     mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN || '';
 
     if (mapContainerRef.current) {
       mapRef.current = new mapboxgl.Map({
         container: mapContainerRef.current,
-        style: 'mapbox://styles/mapbox/streets-v11', // Add a mapbox style
-        center: [-74.0242, 40.6941],
-        zoom: 10.12,
+        style: 'mapbox://styles/mapbox/dark-v11',
+        center: center,
+        zoom: zoom,
+      });
+    }
+
+    if (mapRef.current) {
+      mapRef.current.on('move', () => {
+        // get the current center coordinates and zoom level from the map
+        const mapCenter = mapRef.current!.getCenter();
+        const mapZoom = mapRef.current!.getZoom();
+
+        // update state
+        setCenter([mapCenter.lng, mapCenter.lat]);
+        setZoom(mapZoom);
       });
     }
 
@@ -30,6 +55,13 @@ const Contact: FC = () => {
 
   return (
     <div className="contact-page">
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} |
+        Zoom: {zoom.toFixed(2)}
+      </div>
+      <button className="reset-button" onClick={handleButtonClick}>
+        Reset
+      </button>
       <div id="map-container" ref={mapContainerRef} />
     </div>
   );
